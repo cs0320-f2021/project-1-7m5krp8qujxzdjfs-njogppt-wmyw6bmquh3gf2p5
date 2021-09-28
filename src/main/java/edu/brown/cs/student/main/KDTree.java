01308c04468db0d75cfde0186b62b5c9230a7726
Futen Wang <futen_wang@brown.edugit config --global user.email futen.wang@gmail.com>
2021-09-28 19:24:27 -0400

    add node class

--- src/main/java/edu/brown/cs/student/main/KDTree.java
+++ src/main/java/edu/brown/cs/student/main/KDTree.java
@@ -1,4 +1,15 @@
 package edu.brown.cs.student.main;
 
+import org.w3c.dom.Node;
+
+import java.util.*;
+
 public class KDTree {
+  private int _dimensions;
+  private Node _root = null;
+
+  public KDTree(int _dimensions, ) {
+
+  }
+
 }
