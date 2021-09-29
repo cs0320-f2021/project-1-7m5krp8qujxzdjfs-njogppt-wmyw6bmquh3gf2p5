52ad0b30be855583ac3683ef2a403a048ef52067
Amanda-Levy <amanda_levy@brown.edu>
2021-09-29 12:02:38 -0400

    fix: comparator for user class

--- src/main/java/edu/brown/cs/student/User/User.java
+++ src/main/java/edu/brown/cs/student/User/User.java
@@ -1,10 +1,14 @@
+package edu.brown.cs.student.User;
+
 import edu.brown.cs.student.main.Comparator;
 import edu.brown.cs.student.main.Node;
+
+import java.util.Collections;
 
 /**
  * A class that stores user info
  */
-public class User implements Comparable<User> {
+public class User {
 
   private final int id;
   private int weight;
@@ -42,16 +46,40 @@ public class User implements Comparable<User> {
   }
 
 
-  @Override
-  public int compareTo(User o) {
-    if(this.compareTo(o.last) > 0) return 1;
-    if(this.last.compareTo(o.last) < 0) return -1;
+}
 
-    if(this.first.compareTo(o.first) > 0) return 1;
-    if(this.first.compareTo(o.first) < 0) return -1;
+static class UserComparatorByDimension extends Comparator<User> {
+  int depth;
+  int currentDepth;
+  int k;
+
+
+  UserComparatorByDimension(int depth, int currentDepth, int k) {
+    this.depth = depth;
+    this.currentDepth = currentDepth;
+    this.k = k;
+  }
+
+  @Override
+  public int compare(User o1, User o2) {
+    // TODO: modify depending on caseMatters
+    if(o1.getDimensionValue(this.depth, this.currentDepth, this.k) > (o2.getDimensionValue(this.depth, this.currentDepth, this.k))) return 1;
+    if(o1.getDimensionValue(this.depth, this.currentDepth, this.k) > (o2.getDimensionValue(this.depth, this.currentDepth, this.k))) return -1;
     return 0;
   }
 
-}
 
+  public static <T extends Comparable<T>> void sortSomeRecords(Comparator<T> comp, List<T> lst) {
+    for(int i=0; i<lst.size(); i++) {
+      for(int j=i+1; j<lst.size(); j++) {
+        if(comp.compare(lst.get(j), lst.get(i)) < 0) {
+          T swap = lst.get(i);
+          lst.set(i, lst.get(j));
+          lst.set(j, swap);
+        }
+      }
+    }
+  }
 
+  initial instance of comparator
+Collections.sort(Comparator<T> comp, List<T> lst);
--- src/main/java/edu/brown/cs/student/main/Comparator.java
+++ src/main/java/edu/brown/cs/student/main/Comparator.java
@@ -10,35 +10,3 @@ public class Comparator<V> {
     return 0;
   }
 }
-static class UserComparatorByDimension implements Comparator<User> {
-  int depth;
-  int currentDepth;
-  int k;
-
-
-  UserComparatorByDimension(int depth, int currentDepth, int k) {
-    this.depth = depth;
-    this.currentDepth = currentDepth;
-    this.k = k;
-  }
-
-  @Override
-  public int compare(User o1, User o2) {
-    // TODO: modify depending on caseMatters
-    if(o1.getDimensionValue(this.depth, this.currentDepth, this.k).compareTo(o2.getDimensionValue(this.depth, this.currentDepth, this.k)) > 0) return 1;
-    if(o1.getDimensionValue(this.depth, this.currentDepth, this.k).compareTo(o2.getDimensionValue(this.depth, this.currentDepth, this.k)) < 0) return -1;
-    return 0;
-  }
-
-
-  public static <T extends Comparable<T>> void sortSomeRecords(Comparator<T> comp, List<T> lst) {
-    for(int i=0; i<lst.size(); i++) {
-      for(int j=i+1; j<lst.size(); j++) {
-        if(comp.compare(lst.get(j), lst.get(i)) < 0) {
-          T swap = lst.get(i);
-          lst.set(i, lst.get(j));
-          lst.set(j, swap);
-        }
-      }
-    }
-  }
