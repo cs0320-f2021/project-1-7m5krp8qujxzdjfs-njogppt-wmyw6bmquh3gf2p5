130dd29f01612ab32a0d650ea7cb2218e05aca3b
Futen Wang <futen_wang@brown.edugit config --global user.email futen.wang@gmail.com>
2021-10-11 21:01:08 -0400

    update KDTree

--- src/main/java/edu/brown/cs/student/main/KDTree/Node.java
+++ src/main/java/edu/brown/cs/student/main/KDTree/Node.java
@@ -2,18 +2,18 @@ package edu.brown.cs.student.main.KDTree;
 
 public class Node<V> {
   
-  private final V _value;
-  private final Node<V> _left;
-  private final Node<V> _right;
+  private V _value;
+  private Node<V> _left;
+  private Node<V> _right;
 
-  private NodeValue _key; // generic
-  private int _depth = 0; // location of this node within the tree
-
-  private static int _k = 0; // the dimensions of the space (ex. 3-dimensional space)
-
-  private Node<NodeValue> _parent = null;
-  private Node<NodeValue> _lesser = null; // left child node
-  private Node<NodeValue> _greater = null; // right child node
+//  private NodeValue _key; // generic
+//  private int _depth = 0; // location of this node within the tree
+//
+//  private static int _k = 0; // the dimensions of the space (ex. 3-dimensional space)
+//
+//  private Node<NodeValue> _parent = null;
+//  private Node<NodeValue> _lesser = null; // left child node
+//  private Node<NodeValue> _greater = null; // right child node
 
 
   public Node(V value, Node<V> left, Node<V> right) {
@@ -22,11 +22,11 @@ public class Node<V> {
     this._right = right;
   }
 
-  public Node(NodeValue key, int depth, int k) {
-    this._key = key;
-    this._k = k;
-    this._depth = depth;
-  }
+//  public Node(NodeValue key, int depth, int k) {
+//    this._key = key;
+//    this._k = k;
+//    this._depth = depth;
+//  }
 
   
   
@@ -37,25 +37,25 @@ public class Node<V> {
   
   public Node<V> getRight() { return _right; }
   
-  public Node<NodeValue> getParent() { return _parent;}
-  
-  public void setParent(Node<NodeValue> parent) { this._parent = parent; }
-
-  public Node<NodeValue> getLesser() { return _lesser; }
-
-  public void setLesser(Node<NodeValue> lesser) { this._lesser = lesser; }
-
-  public Node<NodeValue> getGreater() { return _greater; }
-
-  public void setGreater(Node<NodeValue> greater) { this._greater = greater; }
-
-  public int getDepth() { return _depth; }
-  
-  public void setDepth(int depth) { this._depth = depth; }
-
-  public NodeValue getKey() { return _key; }
-
-  public void setKey(NodeValue key) { this._key = key; }
+//  public Node<NodeValue> getParent() { return _parent;}
+//
+//  public void setParent(Node<NodeValue> parent) { this._parent = parent; }
+//
+//  public Node<NodeValue> getLesser() { return _lesser; }
+//
+//  public void setLesser(Node<NodeValue> lesser) { this._lesser = lesser; }
+//
+//  public Node<NodeValue> getGreater() { return _greater; }
+//
+//  public void setGreater(Node<NodeValue> greater) { this._greater = greater; }
+//
+//  public int getDepth() { return _depth; }
+//
+//  public void setDepth(int depth) { this._depth = depth; }
+//
+//  public NodeValue getKey() { return _key; }
+//
+//  public void setKey(NodeValue key) { this._key = key; }
 
 
 }
