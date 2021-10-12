4daadc33b3b7e53fccfec94b3e8738177bcfec81
Futen Wang <futen_wang@brown.edugit config --global user.email futen.wang@gmail.com>
2021-10-12 13:48:19 -0400

    update KDTree

--- src/main/java/edu/brown/cs/student/main/KDTree/KDTree.java
+++ src/main/java/edu/brown/cs/student/main/KDTree/KDTree.java
@@ -1,71 +1,68 @@
 package edu.brown.cs.student.main.KDTree;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.ArrayList; import java.util.Comparator; import java.util.List;
 
 public class KDTree<V> {
   
-  private int _dimensions;
-  private Node<User<V>> _root;
+  private int _dimensions; // the dimensions of the space (ex. 3-dimensional space)
+//  private Integer _k;
+  private Node<NodeValue<V>> _root; // root node
+//  private Node<V> _root;
 
-//  private Node<V> _root; // root node
-//  private Integer _k; // the dimensions of the space (ex. 3-dimensional space)
-
-  public KDTree(int dimensions, List<User<V>> users) {
+  public KDTree(int dimensions, List<NodeValue<V>> nodeValues) {
     this._dimensions = dimensions;
-    this._root = generateOnStart(new ArrayList<>(users));
+    this._root = generateOnStart(new ArrayList<>(nodeValues));
   }
   
 //  public KDTree(Integer k) { this._k =  k; }
 
-  public Node<User<V>> getRoot() { return _root; }
+  public Node<NodeValue<V>> getRoot() { return _root; }
   
-  public Node<User<V>> generateOnStart(List<User<V>> users) { return newLevel(users, 1); }
+  public Node<NodeValue<V>> generateOnStart(List<NodeValue<V>> nodeValues) { return newLevel(nodeValues, 1); }
   
-  public Node<User<V>> newLevel(List<User<V>> usersLeft, int dim) {
-    if (usersLeft.size() != 0) {
-      int indexMid = usersLeft.size() / 2;
-      Comparator<User<V>> byDimension = Comparator.comparingDouble(users -> users.getUserVal(dim));
-      usersLeft.sort(byDimension);
+  public Node<NodeValue<V>> newLevel(List<NodeValue<V>> nodeValuesLeft, int dim) {
+    if (nodeValuesLeft.size() != 0) {
+      int indexMid = nodeValuesLeft.size() / 2;
+      Comparator<NodeValue<V>> byDimension = Comparator.comparingDouble(nodeValues -> nodeValues.getNodeValueVal(dim));
+      nodeValuesLeft.sort(byDimension);
 
-      User<V> medianUser = usersLeft.get(indexMid); // find median user, users lesser, users greater than median
+      NodeValue<V> medianNodeValues = nodeValuesLeft.get(indexMid); // find median nodeValue, nodeValues lesser, nodeValues greater than median
 
-//      List<List<User<V>>> splitResult = Utils.splitList(remainingUsers, middleIndex);
+//      List<List<NodeValue<V>>> splitResult = Utils.splitList(remainingNodeValues, middleIndex);
       int i = 0;
-      List<User<V>> first = new ArrayList<>();
-      List<User<V>> second = new ArrayList<>();
-      for (User<V> element : usersLeft) {
+      List<NodeValue<V>> first = new ArrayList<>();
+      List<NodeValue<V>> second = new ArrayList<>();
+      for (NodeValue<V> element : nodeValuesLeft) {
         if (i < indexMid) { first.add(element); }
         else if (i > indexMid) { second.add(element); }
         i++;
       }
-      List<List<User<V>>> finalResult = new ArrayList<>();
+      List<List<NodeValue<V>>> finalResult = new ArrayList<>();
       finalResult.add(first);
       finalResult.add(second);
 
-      List<User<V>> lesserUsers = finalResult.get(0);
-      List<User<V>> greaterUsers = finalResult.get(1);
+      List<NodeValue<V>> lesserNodeValues = finalResult.get(0);
+      List<NodeValue<V>> greaterNodeValues = finalResult.get(1);
 
       // calculate next dimension
       int dimNext;
       if (dim + 1 > _dimensions) { dimNext = 1; }
       else { dimNext = dim + 1; }
 
-      return new Node<>(medianUser, newLevel(lesserUsers, dimNext), newLevel(greaterUsers, dimNext));
+      return new Node<>(medianNodeValues, newLevel(lesserNodeValues, dimNext), newLevel(greaterNodeValues, dimNext));
     } else { return new Node<>(null, null, null); }
   }
 
-//  public Node<NodeValue> getMidpointToNode(int depth, List<NodeValue> userList) {
-//    NodeValueHandlerClass userSorter = new NodeValueHandlerClass();
-//    List<NodeValue> sortedUserList = userSorter.sortedByDimension(depth, userList);
-//    int midPointIndex = (sortedUserList.size() / 2) - 1;
-//    User userToNode = sortedUserList.get(midPointIndex);
+//  public Node<NodeValue> getMidpointToNode(int depth, List<NodeValue> nodeValueList) {
+//    NodeValueHandlerClass nodeValueSorter = new NodeValueHandlerClass();
+//    List<NodeValue> sortedNodeValueList = nodeValueSorter.sortedByDimension(depth, nodeValueList);
+//    int midPointIndex = (sortedNodeValueList.size() / 2) - 1;
+//    NodeValue nodeValueToNode = sortedNodeValueList.get(midPointIndex);
 //
-//    List<NodeValue> leftList = sortedUserList.subList(0, midPointIndex);
-//    List<NodeValue> rightList = sortedUserList.subList(midPointIndex + 1, sortedUserList.size());
+//    List<NodeValue> leftList = sortedNodeValueList.subList(0, midPointIndex);
+//    List<NodeValue> rightList = sortedNodeValueList.subList(midPointIndex + 1, sortedNodeValueList.size());
 //
-//    return this.createNode(sortedUserList.get(midPointIndex), this.getMidpointToNode(depth + 1,
+//    return this.createNode(sortedNodeValueList.get(midPointIndex), this.getMidpointToNode(depth + 1,
 //        leftList), this.getMidpointToNode(depth + 1, rightList), depth);
 //  }
 //
--- src/main/java/edu/brown/cs/student/main/KDTree/NodeValue.java
+++ src/main/java/edu/brown/cs/student/main/KDTree/NodeValue.java
@@ -1,8 +1,16 @@
 package edu.brown.cs.student.main.KDTree;
 
-public interface NodeValue {
-    int getSecondAttribute();
-    int getFourthAttribute();
-    int getFifthAttribute();
-    int getDimensionValue(int currentDepth);
+import java.util.List;
+
+// Interface of a User with a specified type ID.
+// @param <V> Any type for the ID of the user that is specified when constructing a User.
+public interface NodeValue<V> {
+    V getId(); // return the id of type with which the User was created
+    List<Double> getNodeValue(); // get all user values of the User, a list of as many numbers as dimensions
+    Double getNodeValueVal(int dim); // get the node value at the dimension requested
+
+//    int getSecondAttribute();
+//    int getFourthAttribute();
+//    int getFifthAttribute();
+//    int getDimensionValue(int currentDepth);
 }
