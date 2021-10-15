72d729ce5cca9c4d90e2131fd7b4d64e65dc3f0f
njogppt <90659814+njogppt@users.noreply.github.com>
2021-10-15 11:31:35 -0400

    Update KDTreeTest.java

--- src/test/java/edu/brown/cs/student/main/KDTreeTest.java
+++ src/test/java/edu/brown/cs/student/main/KDTreeTest.java
@@ -13,24 +13,24 @@ import java.util.List;
 
 public class KDTreeTest {
 
-  @Test
-  public void makeKDTree() {
-    NodeValue<Integer> one = new TestObject(1, 4.0, 7.0, 4.0);
-    NodeValue<Integer> two = new TestObject(2, 7.0, 1.0, 3.0);
-    NodeValue<Integer> three = new TestObject(3, 2.0, 9.0, 4.0);
+    @Test
+    public void makeKDTree() {
+        NodeValue<Integer> one = new TestObject(1, 4.0, 7.0, 4.0);
+        NodeValue<Integer> two = new TestObject(2, 7.0, 1.0, 3.0);
+        NodeValue<Integer> three = new TestObject(3, 2.0, 9.0, 4.0);
 
-    ArrayList<NodeValue<Integer>> list = new ArrayList<>();
-    list.add(one);
-    list.add(two);
-    list.add(three);
+        ArrayList<NodeValue<Integer>> list = new ArrayList<>();
+        list.add(one);
+        list.add(two);
+        list.add(three);
 
-    KDTree kdt = new KDTree(3, list);
+        KDTree kdt = new KDTree(3, list);
 
-    NodeValue<Integer> target = new TestObject(999, 7.0, 1.0, 3.1);
-    List<NodeValue<Integer>> result = kdt.getKNN(1, target);
+        NodeValue<Integer> target = new TestObject(999, 7.0, 1.0, 3.1);
+        List<NodeValue<Integer>> result = kdt.getKNN(1, target);
 
-    assertEquals(7.0, result.get(0).getSingleNodeValue(1), .01);
-    assertEquals(1.0, result.get(0).getSingleNodeValue(2), .01);
-    assertEquals(3.0, result.get(0).getSingleNodeValue(3), .01);
-  }
+        assertEquals(7.0, result.get(0).getSingleNodeValue(1), .01);
+        assertEquals(1.0, result.get(0).getSingleNodeValue(2), .01);
+        assertEquals(3.0, result.get(0).getSingleNodeValue(3), .01);
+    }
 }
