068d6a5ff751a1c7ad5e58d60081e75de2b43f86
njogppt <90659814+njogppt@users.noreply.github.com>
2021-10-19 12:23:07 -0400

    Update NodeValue.java

--- src/main/java/edu/brown/cs/student/main/KDTree/NodeValue.java
+++ src/main/java/edu/brown/cs/student/main/KDTree/NodeValue.java
@@ -5,8 +5,8 @@ import java.util.List;
 // interface for a user
 public interface NodeValue<V> {
   V getId(); // return the generic object's identification
-  List<Double> getNodeValue(); // a list of as many numbers as dimensions (i.e [x, y, z] )
-  Double getSingleNodeValue(int dim); // get the nodeValue value at the dimension requested (i.e. x or y or z)
+  List<Double> getNodeValue(); // return a list of its dimension values (i.e [x, y, z] )
+  Double getSingleNodeValue(int dim); // get the value at the requested dimension (i.e. x or y or z)
   // NOTE: should throw indexOutOfBound exception if dim is not accessible (ex. getSingleNodeValue(4) in 3-D space)
   // so recommend storing x,y,z in list of some sort
 
